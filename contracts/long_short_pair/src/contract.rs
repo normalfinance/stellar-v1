3e4ba53cd37fd03d0fc1060e805747e14df6bcae
Joshua Blew <joshua@normalfinance.io>
2026-01-12 11:15:07 -0600

    fix: Contract Lifecycle & State: Unprotected initialize enables contract takeover

--- contracts/long_short_pair/src/contract.rs
+++ contracts/long_short_pair/src/contract.rs
@@ -35,11 +35,15 @@ pub struct LongShortPair;
 #[contractimpl]
 impl LongShortPairTrait for LongShortPair {
     fn initialize(e: Env, params: PairParams) {
+        params.admin.require_auth();
+
         let access_control = AccessControl::new(&e);
         if access_control.get_role_safe(&Role::Admin).is_some() {
             panic_with_error!(&e, LongShortPairError::AlreadyInitialized);
         }
         access_control.set_role_address(&Role::Admin, &params.admin);
+        access_control.set_role_address(&Role::PauseAdmin, &params.admin);
+        access_control.set_role_address(&Role::EmergencyAdmin, &params.admin);
 
         crate::storage::set_asset(&e, &params.asset);
         crate::storage::set_status(&e, &PairStatus::Active);
