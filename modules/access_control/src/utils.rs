a27bfdb04026c4c0784726a421f1873069c0d76e
Joshua Blew <joshua@normalfinance.io>
2026-01-12 13:32:38 -0600

    fix(Access): Signature & Authentication: Role checks omit signer authentication enforcement

--- modules/access_control/src/utils.rs
+++ modules/access_control/src/utils.rs
@@ -4,6 +4,7 @@ use crate::role::Role;
 use soroban_sdk::{panic_with_error, Address, Env};
 
 pub fn require_rewards_admin_or_owner(e: &Env, address: &Address) {
+    address.require_auth();
     let access_control = AccessControl::new(e);
     let _ = access_control.address_has_role(address, &Role::Admin)
         || access_control.address_has_role(address, &Role::RewardsAdmin)
@@ -11,6 +12,7 @@ pub fn require_rewards_admin_or_owner(e: &Env, address: &Address) {
 }
 
 pub fn require_operations_admin_or_owner(e: &Env, address: &Address) {
+    address.require_auth();
     let access_control = AccessControl::new(e);
     let _ = access_control.address_has_role(address, &Role::OperationsAdmin)
         || access_control.address_has_role(address, &Role::Admin)
@@ -18,6 +20,7 @@ pub fn require_operations_admin_or_owner(e: &Env, address: &Address) {
 }
 
 pub fn require_system_fee_admin_or_owner(e: &Env, address: &Address) {
+    address.require_auth();
     let access_control = AccessControl::new(e);
     let _ = access_control.address_has_role(address, &Role::SystemFeeAdmin)
         || access_control.address_has_role(address, &Role::Admin)
@@ -25,6 +28,7 @@ pub fn require_system_fee_admin_or_owner(e: &Env, address: &Address) {
 }
 
 pub fn require_pause_or_emergency_pause_admin_or_owner(e: &Env, address: &Address) {
+    address.require_auth();
     let access_control = AccessControl::new(e);
     let _ = access_control.address_has_role(address, &Role::PauseAdmin)
         || access_control.address_has_role(address, &Role::EmergencyPauseAdmin)
@@ -33,6 +37,7 @@ pub fn require_pause_or_emergency_pause_admin_or_owner(e: &Env, address: &Addres
 }
 
 pub fn require_pause_admin_or_owner(e: &Env, address: &Address) {
+    address.require_auth();
     let access_control = AccessControl::new(e);
     let _ = access_control.address_has_role(address, &Role::PauseAdmin)
         || access_control.address_has_role(address, &Role::Admin)
@@ -40,6 +45,7 @@ pub fn require_pause_admin_or_owner(e: &Env, address: &Address) {
 }
 
 pub fn require_owner(e: &Env, address: &Address) {
+    address.require_auth();
     let access_control = AccessControl::new(e);
     let _ = access_control.address_has_role(address, &Role::Admin)
         || panic_with_error!(e, AccessControlError::Unauthorized);
