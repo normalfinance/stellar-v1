922478593396c47e508e2192e4c55731c1fb61e3
Joshua Blew <joshua@normalfinance.io>
2026-02-11 16:56:07 -0600

    refactor(pair): add better access control

--- modules/types/src/pair.rs
+++ modules/types/src/pair.rs
@@ -1,10 +1,17 @@
-use soroban_sdk::{contracttype, Address, Symbol};
+use soroban_sdk::{contracttype, Address, Symbol, Vec};
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct PairParams {
-    // Config
+    // Access control
     pub admin: Address,
+    pub emergency_admin: Address,
+    pub operations_admin: Address,
+    pub pause_admin: Address,
+    pub emergency_pause_admins: Vec<Address>,
+    pub rewards_admin: Address,
+
+    // Config
     pub asset: Symbol,
     pub oracle: Address,
 
